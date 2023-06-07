c675e0049af8132f273ab85a0e2d4bcccb0b9bb0
Alejandro Madrigal Leiva <me@alemadlei.tech>
2023-06-06 18:13:01 -0600

    Limpieza de formulario.

--- src/Controller/CrudinController.php
+++ src/Controller/CrudinController.php
@@ -4,46 +4,69 @@ namespace Drupal\crudin\Controller;
 
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Url;
 use Drupal\Core\Link;
 use Drupal\Core\Messenger;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
-class CrudinController extends ControllerBase
-{
-    public function Listing()
-    {
-    
-      $header_table = ['id'=>t('ID'),'name'=>t('Name'),'age'=>t('Age'),'opt'=>t('Operation'),'opt1'=>t('Operation'),];
-      $row = [];
-      
-      $conn = Database::getConnection();
-      
-      $query = $conn->select('crudin','m');
-      $query->fields('m',['id','name','age']);
-      $result = $query->execute()->fetchALL();
-      
-      foreach($result as $value)
-      {
-         $delete = Url::fromUserInput('/crudin/form/delete/'.$value->id);
-         $edit = Url::fromUserInput('/crudin/form/data?id='.$value->id);
-         
-         $row[]= ['id'=>$value->id,'name'=>$value->name,'age'=>$value->age,'opt'=>Link::fromTextAndUrl('Edit',$edit)->toString(),'opt1'=>Link::formTextAndUrl('Delete',$delete)->toString(),];
-      }
-      
-      $add = Url::fromUserInput('/crudin/form/data');
-      
-      $text = "Add User";
-      
-      $data['table'] = ['#type'=>'table','#header'=>$header_table,'#rows'=>$row,'#empty'=>t('No Record Found'),'#caption'=> Link::fromTextAndUrl($text,$add)->toString(),];
-      
-      $this->messenger()->addMessage('Record Listed');
-      
-      return $data;
+class CrudinController extends ControllerBase {
+
+  public function __construct(private readonly Connection $database) {
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static (
+      $container->get('database')
+    );
+  }
+
+
+  public function Listing() {
+
+    $header_table = [
+      'id' => t('ID'),
+      'name' => t('Name'),
+      'age' => t('Age'),
+      'opt' => t('Operation'),
+      'opt1' => t('Operation'),
+    ];
+    $row = [];
+
+    $query = $this->database->select('crudin', 'm');
+    $query->fields('m', ['id', 'name', 'age']);
+    $result = $query->execute()->fetchALL();
+
+    foreach ($result as $value) {
+      $delete = Url::fromRoute('crudin.crudin_form', ['id' => $value->id]);
+      $edit = Url::fromRoute('crudin.crudin_form', ['id' => $value->id]);
+
+      $row[] = [
+        'id' => $value->id,
+        'name' => $value->name,
+        'age' => $value->age,
+        'opt' => Link::fromTextAndUrl('Edit', $edit)->toString(),
+        'opt1' => Link::fromTextAndUrl('Delete', $delete)->toString(),
+      ];
+    }
+
+    $add = Url::fromUserInput('/crudin/form/data');
+
+    $text = "Add User";
+
+    $data['table'] = [
+      '#type' => 'table',
+      '#header' => $header_table,
+      '#rows' => $row,
+      '#empty' => t('No Record Found'),
+      '#caption' => Link::fromTextAndUrl($text, $add)->toString(),
+    ];
+
+    $this->messenger()->addMessage('Record Listed');
+
+    return $data;
+  }
+
 }
-
-}
-
-
-?>
